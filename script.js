27df794c48711d7750e96cf9d8165ee64fa8d915
Tamiris Cristine <tamiris.css@gmail.com>
2021-11-24 20:24:06 +0100

    load countries and filter by name

--- script.js
+++ script.js
@@ -11,52 +11,54 @@ const search = document.querySelector('.fa-search');
 const cardsContainer = document.querySelector('.country-cards-container');
 
 //country list
-const countries = [];
+let countries = [];
 //TODO: load all countries data
+async function getCountries() {
+    // e.preventDefault();
+    const response = await fetch(`https://restcountries.com/v2/all`)
+    const data = await response.json()
+    console.log(data)
+    countries = data;
+    showCountries(countries)
+}
+getCountries();
 
-search.addEventListener('click', getCountry);
-input.addEventListener('keydown', function(e) {
-    if(e.key === "Enter") {
-        getCountry();
-    }
-});
+search.addEventListener('click', filterCountries);
+input.addEventListener('keyup', filterCountries);
 
 //TODO implement a filter function based on the input.value
-function filterCountries(inputValue) {
-    return "a new list with the countries that match the filter"
+function filterCountries() {
+    const filter = input.value.toUpperCase()
+    // const filtered = []
+    // countries.forEach(country => {
+    //     if (country.name.toUpperCase().startsWith(filter)) {
+    //         filtered.push(country)
+    //     }
+    // })
+    const filtered = countries.filter(country => country.name.toUpperCase().startsWith(filter))
+    showCountries(filtered)
 }
 
 function filterCountriesByRegion(region) {
     return "a new list with the countries that match the filter"
 }
 
-function getCountry() {
-    // e.preventDefault();
-    const country = input.value;
-    fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        showCountries(data)
-    })
-    input.value = "";
-}
-
 
 
 function showCountries(data) {
+    cardsContainer.innerHTML = ""
     data.forEach(element => {
         const card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
-        <img src="${data[0].flags.png}"
+        <img src="${element.flags.png}"
         alt="image">
         <div class="country-info">
-            <h3>${data[0].name}</h3>
-            <p>Population: ${data[0].population}</p>
-            <p>Region: ${data[0].region}</p>
-            <p>Capital: ${data[0].capital}</p>
-            <p>Language: ${data[0].languages[0].name}</p>
+            <h3>${element.name}</h3>
+            <p>Population: ${element.population}</p>
+            <p>Region: ${element.region}</p>
+            <p>Capital: ${element.capital}</p>
+            <p>Language: ${element.languages[0].name}</p>
         </div>
         `
         cardsContainer.appendChild(card);
